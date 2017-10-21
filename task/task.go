835f662c99fc67791653277b750c2feb7b16b4dd
L2015023 <l2015023@L2015023deMacBook-Pro.local>
2017-10-22 03:34:11 +0800

    add the tasklog

--- task/task.go
+++ task/task.go
@@ -5,33 +5,126 @@ import (
 	"sir/lib/psutil"
 	"sir/models"
 	"time"
+
+	"log"
+	"os"
+	"os/signal"
+	"sir/lib/config"
+	"sir/models"
+	"syscall"
+
+	readline "github.com/jprichardson/readline-go"
+	"github.com/natefinch/lumberjack"
 )
 
 type TaskRuntime struct {
 	*models.Task
 
-	TaskLogSignal   chan bool
-	TaskStateSignal chan bool
+	TaskStdLogSignal   chan bool
+	TaskErrorLogSignal chan bool
+	TaskStateSignal    chan bool
 }
 
 func NewTaskRuntime(task *models.Task) *TaskRuntime {
 	taskRuntime := &TaskRuntime{
-		Task:            task,
-		TaskLogSignal:   make(chan bool),
-		TaskStateSignal: make(chan bool),
+		Task:               task,
+		TaskStdLogSignal:   make(chan bool),
+		TaskErrorLogSignal: make(chan bool),
+		TaskStateSignal:    make(chan bool),
 	}
 
 	return taskRuntime
-
 }
 
 func (t *TaskRuntime) TaskLog() {
+
+	// deal with std log
+	go func() {
+		logger := &lumberjack.Logger{
+			Filename:   config.DefaultLogPath + "/log.log",
+			MaxSize:    10, // megabytes
+			MaxBackups: 0,
+			MaxAge:     0, //days
+		}
+
+		log.SetOutput(logger)
+
+		readline.ReadLine(t.Task.TaskFlows.StdOut, func(line string) {
+
+			grSignal := make(chan bool)
+
+			select {
+			case <-t.TaskStdLogSignal:
+				grSignal <- true
+				return
+			}
+
+			logger.Write([]byte(line))
+
+			c := make(chan os.Signal, 1)
+			signal.Notify(c, syscall.SIGHUP)
+
+			go func() {
+				for {
+
+					select {
+					case <-grSignal:
+						return
+					}
+
+					<-c
+					logger.Rotate()
+				}
+			}()
+		})
+	}()
+
+	// deal with error log
+	go func() {
+		logger := &lumberjack.Logger{
+			Filename:   config.DefaultLogPath + "/error.log",
+			MaxSize:    10, // megabytes
+			MaxBackups: 0,
+			MaxAge:     0, //days
+		}
+
+		log.SetOutput(logger)
+
+		readline.ReadLine(t.Task.TaskFlows.StdErr, func(line string) {
+
+			grSignal := make(chan bool)
+
+			select {
+			case <-t.TaskErrorLogSignal:
+				grSignal <- true
+				return
+			}
+
+			logger.Write([]byte(line))
+
+			c := make(chan os.Signal, 1)
+			signal.Notify(c, syscall.SIGHUP)
+
+			go func() {
+				for {
+
+					select {
+					case <-grSignal:
+						return
+					}
+
+					<-c
+					logger.Rotate()
+				}
+			}()
+		})
+	}()
 }
 
 func (t *TaskRuntime) TaskStateFunc() {
 	for {
 		select {
-		// kill
+		// killz
 		case <-t.TaskStateSignal:
 			return
 
@@ -51,14 +144,13 @@ func (t *TaskRuntime) TaskStateFunc() {
 			t.TaskState = state
 
 			time.Sleep(time.Second)
-
 		}
 	}
-
 }
 
 func (t *TaskRuntime) Stop() {
-	t.TaskLogSignal <- true
+	t.TaskStdLogSignal <- true
+	t.TaskErrorLogSignal <- true
 	t.TaskStateSignal <- true
 }
 
