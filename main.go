e2abf6dd0bf3354eddb7efacfc695650e14f5d83
Ben <njutree@gmail.com>
2017-10-21 21:46:35 +0800

    make daemon works

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	_ "sir/routers"
 	"github.com/astaxie/beego"
+	"sir/lib/daemon"
+	_ "sir/routers"
 )
 
 func main() {
+	err := daemon.Daemon("/Users/hong/projects/src/sir/std.log", "/Users/hong/projects/src/sir/err.log")
+	beego.Error(err)
+
 	beego.Run()
 }
-
