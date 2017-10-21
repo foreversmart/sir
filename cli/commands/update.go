9b656ad5f4325fa6ec6a53c9ed0ce40653ed4751
Alex (Jinghao) Yan <yanhao1991@gmail.com>
2017-10-22 01:21:31 +0800

    finish update cli cmd

--- cli/commands/update.go
+++ cli/commands/update.go
@@ -1,6 +1,14 @@
 package commands
 
 import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"sir/cli/config"
+	"sir/cli/utils"
+	"sir/lib/httpclient"
+
 	cli "gopkg.in/urfave/cli.v1"
 )
 
@@ -13,5 +21,27 @@ var CmdUpdate = cli.Command{
 }
 
 func ActionUpdate(c *cli.Context) error {
+	taskName := c.Args().First()
+	// 调用api
+	var response map[string]string
+	httpclient.Client.DoJSON(http.MethodPut, config.ApiPath("/task/"+taskName), nil, &response)
+	// TODO handle error
+
+	configPath := response["data"]
+
+	editorPath, _ := exec.LookPath("vi")
+	// TODO handle error
+
+	cmd := exec.Command(editorPath, configPath)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+
+	println()
+	fmt.Println(utils.Style.Success("[INFO]"), "EDIT CONFIG DONE")
+	fmt.Println(utils.Style.Success("[INFO]"), `PLEASE RESTART TASK PROCESS IF YOU WANT TO APPLY NEW CONFIG, BY:
+       $ sir restart`, utils.Style.Title(taskName))
+
 	return nil
 }
