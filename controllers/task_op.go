a8592f981f6229a22bd6e6ecb3a80c1cc9dc2ba4
Ben <njutree@gmail.com>
2017-10-21 20:38:00 +0800

    add task op route

---
+++ controllers/task_op.go
@@ -0,0 +1,21 @@
+package controllers
+
+// @router /task/:name/start [post]
+func (task *TaskController) Start() {
+
+}
+
+// @router /task/:name/restart [post]
+func (task *TaskController) Restart() {
+
+}
+
+// @router /task/:name/stop [post]
+func (task *TaskController) Stop() {
+
+}
+
+// @router /task/:name/send [post]
+func (task *TaskController) Send() {
+
+}
