c66527c54d9ff8c2f5447280ed9c9684e0cef8b9
Ben <njutree@gmail.com>
2017-10-22 03:29:59 +0800

    add task state func

--- task/task.go
+++ task/task.go
@@ -1,6 +1,11 @@
 package task
 
-import "sir/models"
+import (
+	"fmt"
+	"sir/lib/psutil"
+	"sir/models"
+	"time"
+)
 
 type TaskRuntime struct {
 	*models.Task
@@ -10,8 +15,8 @@ type TaskRuntime struct {
 }
 
 func NewTaskRuntime(task *models.Task) *TaskRuntime {
-	taskRuntime := TaskRuntime{
-		task,
+	taskRuntime := &TaskRuntime{
+		Task:            task,
 		TaskLogSignal:   make(chan bool),
 		TaskStateSignal: make(chan bool),
 	}
@@ -23,12 +28,42 @@ func NewTaskRuntime(task *models.Task) *TaskRuntime {
 func (t *TaskRuntime) TaskLog() {
 }
 
-func (t *TaskRuntime) TaskState() {
+func (t *TaskRuntime) TaskStateFunc() {
+	for {
+		select {
+		// kill
+		case <-t.TaskStateSignal:
+			return
 
+		default:
+			state, err := psutil.TaskState(t.Pid)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			if state == nil {
+				state = &models.TaskState{
+					Pid: t.Pid,
+				}
+			}
+
+			// update task state
+			t.TaskState = state
+
+			time.Sleep(time.Second)
+
+		}
+	}
+
+}
+
+func (t *TaskRuntime) Stop() {
+	t.TaskLogSignal <- true
+	t.TaskStateSignal <- true
 }
 
 func (t *TaskRuntime) Run() {
 	go t.TaskLog()
 
-	go t.TaskState()
+	go t.TaskStateFunc()
 }
