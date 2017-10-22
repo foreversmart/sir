0d0d3c6c35e3cecd3aa247980d96283b010d48f8
StaticLove <haiyangyu0117@gmail.com>
2017-10-22 09:15:40 +0800

    refactor the monitor

--- lib/monitor/monitor.go
+++ lib/monitor/monitor.go
@@ -18,6 +18,7 @@ var (
 func handler() http.Handler {
 
 	logBuf := &bytes.Buffer{}
+
 	logger := log.New(logBuf, "", 0)
 
 	return promhttp.HandlerFor(reg, promhttp.HandlerOpts{
@@ -26,48 +27,41 @@ func handler() http.Handler {
 	})
 }
 
+func initGuage(nameSpace, subSystem, name string, labels []string) *prometheus.GaugeVec {
+	return prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: nameSpace,
+			Subsystem: subSystem,
+			Name:      name,
+			Help:      "docstring",
+		},
+		labels,
+	)
+}
+
 func PushMonitorData(state *models.TaskState) {
+
 	reg = prometheus.NewRegistry()
 
-	// init the cpu data
-	CpuData := prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Namespace: "process",
-			Subsystem: "CPU",
-			Name:      "cpu_percent",
-			Help:      "docstring",
-		},
-		[]string{"pid", "unit"},
-	)
+	pid := strconv.Itoa(state.Pid)
 
-	CpuData.WithLabelValues(strconv.Itoa(state.Pid), "%").Set(state.CpuPercent)
+	// init the cpu data
+	cpuData := initGuage("process", "CPU", "cpu_percent", []string{"pid", "unit"})
+
+	cpuData.WithLabelValues(pid, "%").Set(state.CpuPercent)
 
 	// init the mem usage data
-	memUsageData := prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Namespace: "process",
-			Subsystem: "mem",
-			Name:      "mem_usage",
-			Help:      "docstring",
-		},
-		[]string{"pid", "unit"},
-	)
-	memUsageData.WithLabelValues(strconv.Itoa(state.Pid), "Kb").Set(float64(state.Mem))
+	memUsageData := initGuage("process", "mem", "mem_usage", []string{"pid", "unit"})
+
+	memUsageData.WithLabelValues(pid, "Kb").Set(float64(state.Mem))
 
 	// init the mem percent data
-	memPercentData := prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Namespace: "process",
-			Subsystem: "mem",
-			Name:      "mem_percent",
-			Help:      "docstring",
-		},
-		[]string{"pid", "unit"},
-	)
-	memPercentData.WithLabelValues(strconv.Itoa(state.Pid), "%").Set(float64(state.MemPercent))
+	memPercentData := initGuage("process", "mem", "mem_percent", []string{"pid", "unit"})
+
+	memPercentData.WithLabelValues(pid, "%").Set(float64(state.MemPercent))
 
 	// register the data
-	reg.MustRegister(CpuData)
+	reg.MustRegister(cpuData)
 	reg.MustRegister(memUsageData)
 	reg.MustRegister(memPercentData)
 
